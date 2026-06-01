2692812c1eb1d1187b6c7d32e6c06557005030b0
Santhosh <your@gmail.com>
2026-06-01 20:35:23 +0530

    deployment: anonymous auth hardening + firestore rules lock

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:ganesha_2026/app.dart';
 import 'package:ganesha_2026/firebase_options.dart';
 
@@ -13,7 +14,9 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  debugPrint('[FIREBASE] Initialization successful');
+  await FirebaseAuth.instance.signInAnonymously();
+
+  debugPrint('[FIREBASE] Anonymous auth established');
 
   runApp(
     const ProviderScope(
